a835e81738454d011ef3adbda77415b97187020e
BenjaLV <benjalavalla8@gmail.com>
2023-08-22 23:13:21 -0300

    first changes in navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,50 @@
+import Link from 'next/link'
+import React from 'react'
+import Logo from './Logo'
+import { useRouter } from 'next/router';
+import { GitHubIcon, LinkedInIcon } from './Icons';
+import { motion } from 'framer-motion'
+
+interface CustomLinkProps {
+  href: string;
+  title: string;
+  className?: string;
+}
+
+const CustomLink: React.FC<CustomLinkProps> = ({ href, title, className = '' }) => {
+
+  const router = useRouter()
+
+  return (
+    <Link href={href} className={`${className} relative-group`}>
+      {title}
+      <span className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'}`}>
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
+
+function Navbar() {
+  return (
+    <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
+      <nav className='flex items-center space-x-4'>
+        <CustomLink href="/" title="Home" className='mr-4' />
+        <CustomLink href="/about" title="About" className='mx-4' />
+        <CustomLink href="/projects" title="Projects" className='ml-4' />
+      </nav>
+      <nav className='flex items-center space-x-4'>
+        <motion.a href="https://github.com/BenjaLV" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }}>
+          <GitHubIcon className='w-6 h-6' />
+        </motion.a>
+        <motion.a href="https://www.linkedin.com/in/benjam%C3%ADn-la-valla-826ba7245" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }}>
+          <LinkedInIcon className='w-6 h-6' />
+        </motion.a>
+      </nav>
+      <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+        <Logo />
+      </div>
+    </header>)
+}
+
+export default Navbar
