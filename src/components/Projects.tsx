c350ea331e3c8a8705cf512859e3c2edfb002292
BenjaLV <benjalavalla8@gmail.com>
2023-08-29 15:06:08 -0300

    project added

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,17 +6,33 @@ import FeaturedProject from './FeaturedProject';
 const Projects = () => {
     return (
         <Layout className="pt-16">
-            <main className="w-full mb-16 flex flex-col items-center justify-center">
+            <main id='projects' className="w-full mb-16 flex flex-col items-center justify-center">
                 <div className="mb-8">
                     <AnimatedText text="Imagination Trumps Knowledge!" />
                 </div>
                 <div className="grid grid-cols-12 gap-6 md:gap-12">
-                    <div className="col-span-12 md:col-span-6">
+                    <div className="col-span-12 md:col-span-6 mb-8 md:mb-0">
                         <FeaturedProject
                             title="CEIBO-APP"
-                            img="/images/not-shadow-2.png"
+                            img="/images/all-devices-ceibo.png"
                             summary="Application to manage and administer the news of the projects of each client among the users of the company"
                             link="https://github.com/BenjaLV/CEIBO-APP"
+                        />
+                    </div>
+                    <div className="col-span-12 md:col-span-6 mb-8">
+                        <FeaturedProject
+                            title="PokemonApp"
+                            img="/images/pokemon-desktop-2.png"
+                            summary='Pokemon App built with NextJs, you can navigate among the 151 pokémons and select your favorites'
+                            link='https://github.com/BenjaLV/pokemonStaticApp'
+                        />
+                    </div>
+                    <div className="col-span-12 md:col-span-6 mb-8">
+                        <FeaturedProject
+                            title="MovieApp"
+                            img="/images/movie-app-all-devices.png"
+                            summary='Movies app built with React, adapted with a nextflix-like interface, consumes the TMDB api and you can add your favorite movies to your list'
+                            link='https://github.com/BenjaLV/TMDB-Client-P5'
                         />
                     </div>
                 </div>
